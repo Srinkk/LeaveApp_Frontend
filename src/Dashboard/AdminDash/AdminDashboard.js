aeb5e63741594013778364ca6de7ce4f64c17463
Shreya Ganguly <93050853+Srinkk@users.noreply.github.com>
2023-05-01 13:08:34 +0530

    Update AdminDashboard.js

--- src/Dashboard/AdminDash/AdminDashboard.js
+++ src/Dashboard/AdminDash/AdminDashboard.js
@@ -1,44 +1,50 @@
 import NavBarDashBoard from '../../NavBar/NavBarDashboard';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ReceivedMail from './ReceivedMail';
 
-import './style.css'
-
-
-
-import { Link } from 'react-router-dom';
-import { Container } from 'react-dom';
-import SignUp from '../../Forms/SignUp';
-import { useLocation } from 'react-router-dom';
+// import './style.css'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const AdminDashboard = () => {
       
+	const navigate = useNavigate() 
     const location = useLocation()
+
 	const { admin_id, admin_name } = location.state || {}
+
+	if (!admin_id?.length || !admin_name?.length) {
+		navigate('/manual')
+	}
 
 	return (
 		<div>
+			<div className='bagc'>
 			<NavBarDashBoard
 				admin_id = {admin_id}
 				student_id = ''
 			/> 
-			<div className='head  '>
+			<div className='dashboard_bg'>
+				<div className='dashboard_overlay'>
+
+				
+			<div className='head'>
 				<h3 className='welcome'>Welcome {admin_name}</h3>	
 			</div>
+			</div>
 			<div className='container'>
-			<h5>Here is your pending mails to check</h5>
-				
 				
 				<ReceivedMail 
 					adminId = {admin_id}
 				/>
 				
-				</div>
-
+			
+	
 			</div>
-		
+			</div>
+
+		</div>
+		</div>
 	)
 }
 
-export default AdminDashboard
+export default AdminDashboard
